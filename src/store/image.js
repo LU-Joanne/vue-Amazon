a590a6d9823e02f1147f55c40c00ceef517a251a
LU-Joanne <43575964+LU-Joanne@users.noreply.github.com>
2026-05-19 16:09:08 +0800

    modify image route

--- src/store/image.js
+++ src/store/image.js
@@ -1,17 +1,17 @@
-const productImages = import.meta.glob('@/assets/img/*.jpg', {
+const productImages = import.meta.glob('../assets/img/*.jpg', {
   eager: true,
   import: 'default',
 })
 
-const ratingImages = import.meta.glob('@/assets/img/rating/*.png', {
+const ratingImages = import.meta.glob('../assets/img/rating/*.png', {
   eager: true,
   import: 'default',
 })
 
 export const getProductImage = (name) => {
-  return productImages[`/src/assets/img/${name}`]
+  return productImages[`../assets/img/${name}`]
 }
 
 export const getRatingImage = (name) => {
-  return ratingImages[`/src/assets/img/rating/${name}`]
+  return ratingImages[`../assets/img/rating/${name}`]
 }
